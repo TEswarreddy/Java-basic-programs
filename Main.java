580b044203cdba2465030032a945569e14ace609
THATHIREDDY ESWAR REDDY <145130369+TEswarreddy@users.noreply.github.com>
2024-12-17 14:50:25 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,41 @@
 // You are using Java
-class Demo
-{
-    public static void main(String args[])
-    {
-        System.out.println("Hello");
-        int age =10;
-        float marks=78.80f;
-        String name="Eswar";
-        System.out.println(age);
-        System.out.println(marks);
-        System.out.println(name);
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        String s="madam";
+        String t="madam";
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char x=s.charAt(i);
+            if(map.containsKey(x)){
+                map.put(x,map.get(x)+1);
+            }
+            else{
+                map.put(x,1);
+            }
+        }
+         for(int i=0;i<t.length();i++){
+            char x=t.charAt(i);
+            if(map.containsKey(x)){
+                map.put(x,map.get(x)-1);
+                
+            }
+            else{
+                System.out.println("not anagram");
+            }
+        }
+        int c=0;
+        for(int count:map.values()){
+            if(count == 0)
+            {
+                c++;
+            }
+        }
+        if(c==map.size())
+        {
+            System.out.println("Anagram");
+        }
+        
+        
     }
 }
